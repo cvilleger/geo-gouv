bf8683769d0c7b9ac915f727b8ad05aa1203f89a
cvilleger <villeger.c@gmail.com>
2026-01-23 23:16:54 +0100

    feat: Rewrite models to english and Bump to PHP 8.5

--- tests/GeoGouvTest.php
+++ tests/GeoGouvTest.php
@@ -5,27 +5,13 @@ declare(strict_types=1);
 namespace Cvilleger\Test\GeoGouv;
 
 use Cvilleger\GeoGouv\Client;
-use Cvilleger\GeoGouv\Model\Centre;
 use Cvilleger\GeoGouv\Model\Commune;
-use Cvilleger\GeoGouv\Model\CommuneDepartement;
-use Cvilleger\GeoGouv\Model\CommuneRegion;
 use Cvilleger\GeoGouv\Model\Departement;
-use Cvilleger\GeoGouv\Model\Region;
-use Cvilleger\GeoGouv\Provider\CoordinatesProvider;
-use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @internal
  */
-#[CoversClass(Client::class)]
-#[CoversClass(Departement::class)]
-#[CoversClass(Region::class)]
-#[CoversClass(Centre::class)]
-#[CoversClass(Commune::class)]
-#[CoversClass(CommuneDepartement::class)]
-#[CoversClass(CommuneRegion::class)]
-#[CoversClass(CoordinatesProvider::class)]
 final class GeoGouvTest extends TestCase
 {
     public function testGetDepartments(): void
