2019573fd4f9c157bcc9241a96da64c9c02da8e7
Christophe Villeger <cvilleger@users.noreply.github.com>
2026-01-30 19:34:30 +0100

    (draft) feat: Refactor department handling and update file naming conventions (#28)

--- scripts/job.php
+++ scripts/job.php
@@ -1,28 +1,53 @@
 <?php
 
-$apiGouvBaseUrl = "https://geo.api.gouv.fr";
-$apiGouvDepartementsUrl = $apiGouvBaseUrl . "/departements?fields=nom,code,codeRegion,region";
-$departementsFilename = "./resources/departements.json";
-$communesUrlStart = $apiGouvBaseUrl . "/departements/";
-$communesUrlEnd = "/communes?fields=nom,code,codesPostaux,centre,surface,population,departement,region";
+$config = [
+    'apiGouvBaseUrl' => 'https://geo.api.gouv.fr',
+    'apiGouvDepartmentsPath' => '/departements',
+    'apiGouvDepartmentsQuery' => [
+        'fields' => 'nom,code,codeRegion,region',
+    ],
+    'apiGouvCommunesPath' => '/communes',
+    'apiGouvCommunesQuery' => [
+        'fields' => 'nom,code,codesPostaux,centre,surface,population,departement,region',
+    ],
+    'departmentsDataFile' => './resources/departments.json',
+    'communesDataFilePattern' => './resources/department-%s.json',
+];
 
-// Télécharger les départements
-file_put_contents($departementsFilename, file_get_contents($apiGouvDepartementsUrl));
+// Retrieve and save the departments data
+file_put_contents(
+    filename: $config['departmentsDataFile'],
+    data: file_get_contents(
+        filename: $config['apiGouvBaseUrl'].$config['apiGouvDepartmentsPath'].'?'.http_build_query($config['apiGouvDepartmentsQuery']),
+    ),
+);
 
-// Lire et décoder le fichier JSON des départements
-$departementsData = json_decode(file_get_contents($departementsFilename), true, 512, JSON_THROW_ON_ERROR);
+// Read and decode the JSON file of departments
+$departmentsData = json_decode(
+    json: file_get_contents(
+        filename: $config['departmentsDataFile'],
+    ),
+    associative: true,
+    flags: JSON_THROW_ON_ERROR,
+);
 
-// Extraire les codes des départements
-$departementsCodes = array_column($departementsData, 'code');
+// Extract department codes
+$departmentsCodes = array_column(
+    array: $departmentsData,
+    column_key: 'code',
+);
 
-echo "DEPARTEMENTS_CODES: " . implode(', ', $departementsCodes) . PHP_EOL;
+echo 'DEPARTMENTS_CODES:'.implode(', ', $departmentsCodes).PHP_EOL;
 
-// Télécharger les données des communes pour chaque département
-foreach ($departementsCodes as $departementCode) {
-    echo "COMMUNE_CODE: " . $departementCode . PHP_EOL;
-    $communesUrl = $communesUrlStart . $departementCode . $communesUrlEnd;
+$communesUrlStart = $config['apiGouvBaseUrl'].$config['apiGouvDepartmentsPath'];
+$communesUrlEnd = $config['apiGouvCommunesPath'].'?'.http_build_query($config['apiGouvCommunesQuery']);
+
+// Retrieve and save the communes data for each department
+foreach ($departmentsCodes as $departmentCode) {
+    echo 'DEPARTMENT_CODE: '.$departmentCode.PHP_EOL;
+    $communesUrl = $config['apiGouvBaseUrl'].$config['apiGouvDepartmentsPath'].'/'.$departmentCode.$communesUrlEnd;
     $communesData = file_get_contents($communesUrl);
-    $communeFilename = "./resources/commune-departement-" . $departementCode . ".json";
+    $communeFilename = sprintf($config['communesDataFilePattern'], $departmentCode);
     file_put_contents($communeFilename, $communesData);
     usleep(100000); // 0.1 seconde
 }
