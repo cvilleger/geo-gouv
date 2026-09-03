d178e173ee0e578d9062b06315b8a4745f3ba786
Christophe V <cvilleger@users.noreply.github.com>
2026-09-03 22:26:41 +0200

    chore: Add test coverage report (#33)

--- tests/GeoGouvTest.php
+++ tests/GeoGouvTest.php
@@ -6,13 +6,27 @@ namespace Cvilleger\Test\GeoGouv;
 
 use Cvilleger\GeoGouv\Client;
 use Cvilleger\GeoGouv\Exception\NotFoundException;
+use Cvilleger\GeoGouv\Model\Centre;
+use Cvilleger\GeoGouv\Model\Commune;
+use Cvilleger\GeoGouv\Model\CommuneDepartement;
+use Cvilleger\GeoGouv\Model\CommuneRegion;
+use Cvilleger\GeoGouv\Model\Departement;
+use Cvilleger\GeoGouv\Model\Region;
+use Cvilleger\GeoGouv\Provider\CoordinatesProvider;
+use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @internal
- *
- * @coversNothing
  */
+#[CoversClass(Client::class)]
+#[CoversClass(CoordinatesProvider::class)]
+#[CoversClass(Centre::class)]
+#[CoversClass(Commune::class)]
+#[CoversClass(CommuneDepartement::class)]
+#[CoversClass(CommuneRegion::class)]
+#[CoversClass(Departement::class)]
+#[CoversClass(Region::class)]
 final class GeoGouvTest extends TestCase
 {
     public function testGetDepartmentsIsNotEmpty(): void
